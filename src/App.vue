11b48e0ac039576793636382c8a9aaa3df8b4423
ibnu <ekmalhafizhqoula987@gmail.com>
2024-06-13 00:11:18 +0700

    UAS fifth commit

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,45 @@
 <script setup>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { useTodoStore } from './store/todoStore';
-
-const router = useRouter();
-const selectedMenu = ref('todos');
-const TodoStore = useTodoStore();
-
-const navigateToTodos = () => {
-  selectedMenu.value = 'todos';
-  router.push('/todos');
-};
-
-const navigateToPost = () => {
-  selectedMenu.value = 'post';
-  router.push('/post');
-};
-
-const navigateToAlbums = () => {
-  selectedMenu.value = 'albums';
-  router.push('/albums');
-};
 </script>
 
 <template>
-  <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-info" id="nav">
-      <button class="btn btn-light" @click="navigateToTodos">Todos</button>
-      <button class="btn btn-light" @click="navigateToPost">Post</button>
-      <button class="btn btn-light" @click="navigateToAlbums">Albums</button>
-    </nav>
-    <RouterView />
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <h1 class="navbar-brand"><i class="bi bi-cpu-fill"></i>  Ibnu Rizan</h1>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <router-link to="/todos" class="nav-link">Todos  <i class="bi bi-list-task"></i></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/post" class="nav-link">Post  <i class="bi bi-file-post"></i></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/albums" class="nav-link">Albums  <i class="bi bi-journal-album"></i></router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <router-view />
 </template>
 
 <style scoped>
-#nav {
-  display: flex;
-  justify-content: center;
+.navbar-brand {
+  font-weight: bold;
 }
-#nav button {
-  margin: 0 10px;
+
+.nav-link {
+  font-size: 18px;
+  margin-left: 20px;
+  font-weight: bold;
+}
+
+.nav-link:hover {
+  color: #ffffff;
+  text-decoration: none;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -4,11 +4,13 @@ import Post from './components/UserPosts.vue';
 import Albums from './components/Albums.vue';
 import AlbumPhotos from './components/AlbumPhotos.vue';
 
+
 const routes = [
   { path: '/todos', name: 'Todos', component: TodoList },
   { path: '/post', name: 'Post', component: Post },
   { path: '/albums', name: 'Albums', component: Albums },
-  { path: '/albums/:albumId', name: 'AlbumPhotos', component: AlbumPhotos, props: true }
+  { path: '/albums/:albumId', name: 'AlbumPhotos', component: AlbumPhotos, props: true },
+  
 ];
 
 
